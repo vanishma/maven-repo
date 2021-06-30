6facebde28bb78c187453d4633557cea539e724a
vanish <maqinghua02@126.com>
2021-06-30 08:21:46 +0800

    add JsonBean.java ResultEnum.java

---
+++ src/main/java/com/maqh/utils/json/JsonBean.java
@@ -0,0 +1,64 @@
+package com.maqh.utils.json;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 统一结果封装类
+ * </p>
+ *
+ * @author maqh
+ * @version 1.0
+ * @since 2021-05-11
+ */
+@Data
+public class JsonBean<T> implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Integer code = 200;
+    private String message = "success";
+    private T data;
+
+    public JsonBean() {
+    }
+
+    public JsonBean(T data) {
+        this.data = data;
+    }
+
+    public JsonBean(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public JsonBean(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> JsonBean<T> success() {
+        return new JsonBean<>();
+    }
+
+    public static <T> JsonBean<T> success(T o) {
+        return new JsonBean<>(o);
+    }
+
+    public static <T> JsonBean<T> success(ResultEnum result) {
+        return new JsonBean<>(result.getCode(), result.getMsg());
+    }
+
+    public static <T> JsonBean<T> err() {
+        return new JsonBean<>(ResultEnum.SAVE_ERROR.getCode(), ResultEnum.SAVE_ERROR.getMsg());
+    }
+
+    public static <T> JsonBean<T> err(ResultEnum result, T o) {
+        return new JsonBean<>(result.getCode(), result.getMsg(), o);
+    }
+
+    public static <T> JsonBean<T> err(ResultEnum result) {
+        return new JsonBean<>(result.getCode(), result.getMsg());
+    }
+}
---
+++ src/main/java/com/maqh/utils/json/ResultEnum.java
@@ -0,0 +1,47 @@
+package com.maqh.utils.json;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author maqh
+ * @version 1.0
+ * @since 2021-05-25
+ */
+public enum ResultEnum {
+    //状态码对应信息
+    SUCCESS(200, "成功"),
+    NOT_AUTHORITY(401, "非法访问"),
+    USER_ALREADY_EXISTS(40001, "该用户名已存在"),
+    NOSESSION(40002, "未登录,或session过期"),
+    USER_NOT_LOGIN(40003, "账号被禁用"),
+    USER_USERNAME_PASSWORD_EXISTS(40004, "账号或密码错误"),
+    SAVE_ERROR(40005, "保存错误"),
+    NOT_DELETE(40006, "无法删除"),
+    HIVE_NOT_DELETE(40007, "已经被使用,无法被删除"),
+    OLDPASSWORD_ERROR(40008, "老密码错误");
+
+    /**
+     * 状态码
+     */
+    private final Integer code;
+
+    /**
+     * 状态信息
+     */
+    private final String msg;
+
+    ResultEnum(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
